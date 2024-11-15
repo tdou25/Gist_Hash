8fb2ceae5c5eccd1aa07ae3fda207cf13825c4de
tdou25 <thomasyd1120@gmail.com>
2024-11-15 01:04:25 -0500

    experiment with basic swing display

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("Hello, World!");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello, World!", SwingConstants.CENTER);
+
+        frame.add(label);
+
+        frame.setSize(300, 200);
+
+        frame.setVisible(true);
     }
 }
