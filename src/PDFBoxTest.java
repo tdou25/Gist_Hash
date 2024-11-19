d1822887d21cb672bcc66eceb08242668bf1e5de
tdou25 <thomasyd1120@gmail.com>
2024-11-19 15:38:31 -0500

    attempting to modify test class for pdfbox 3.0

--- src/PDFBoxTest.java
+++ src/PDFBoxTest.java
@@ -1,16 +1,19 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.Loader;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 public class PDFBoxTest {
     public static void main(String[] args) {
-        try {
+        PDDocument document = new PDDocument();
+        File file = new File("pdf_files/example.pdf");
+        PDDocument doc1 = Loader.loadPDF(file);
 
-            PDDocument document = PDDocument.load(Paths.get("example.pdf"));
-            System.out.println("Number of pages: " + document.getNumberOfPages());
-            document.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+//        PDDocument document = PDDocument.load(Paths.get("example.pdf"));
+//        System.out.println("Number of pages: " + document.getNumberOfPages());
+//        document.close();
     }
 }
